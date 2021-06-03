220df1f987faa2ecd04fa449bba479d46c6d8581
Arnav-png <79275041+Arnav-png@users.noreply.github.com>
2021-06-03 22:44:18 +0530

    PAYMENT

---
+++ index.js
@@ -0,0 +1,11 @@
+{
+    /* <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HIZbl2zPgX3x5P" async> </script> </form>
+
+
+*/
+}
+
+
+
+
+{ /* <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HIZbl2zPgX3x5P" async> </script> </form> */ }
